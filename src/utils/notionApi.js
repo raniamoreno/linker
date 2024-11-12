ee8dc6ff361d86fc51773d15e4f01b1d88287643
Anna Muratova <12412630+raniamoreno@users.noreply.github.com>
2024-11-12 15:47:57 +0000

    Ready for deployment

--- src/utils/notionApi.js
+++ src/utils/notionApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3001/api/notion/',
+  baseURL: '/api/notion/',
   headers: {
     'Content-Type': 'application/json',
   },
